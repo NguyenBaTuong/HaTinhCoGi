1df0ab4c6bb4a1a08a9816c24c37b473bd5dddcf
NguyenBaTuong <61742750+NguyenBaTuong@users.noreply.github.com>
2021-10-06 09:46:50 +0700

    ad new tab manage place on infoUser__page

--- js/infoUser.js
+++ js/infoUser.js
@@ -25,38 +25,6 @@ info__tabs.forEach((tab, index) => {
   };
 });
 
-// function active() {
-//   var active__content = document.getElementById('active__content');
-
-//   if (active__content.style.display === 'none') {
-//     active__content.style.display = 'flex';
-//   } else {
-//     active__content.style.display = 'none';  
-//   }
-// } 
-
-// document.addEventListener("click", function(evt) {
-//   // var active__content = document.getElementById('active__content');
-//   // var active = document.getElementById('active');
-//   //     targetElement = evt.target;  
-//   var active__content = document.getElementsByClassName('info__nav-pane-active-item-btn-mess');
-//   var active = document.getElementsByClassName('info__nav-pane-active-item-btn');
-//       targetElement = evt.target;  
-  
-//   do {
-//       if (targetElement == active) {
-//           active__content.style.display = 'flex';
-//           return;
-//       }
-//       targetElement = targetElement.parentNode;
-//   } while (targetElement);
-
-//   active__content.style.display = 'none';
-
-// });
-
-//show avt and update
-
 var avt__block = document.getElementById('avt__block');
 
 avt__block.onclick = function () {
@@ -90,7 +58,6 @@ cancel__avt.onclick = evt => {
   const [file] = avt__up.files
   if (file) {
     avt__origin.src = link__avt;
-    // avt__other.src = link__avt;
   }
 }
 
@@ -180,3 +147,123 @@ togglePassword3.addEventListener('click', function (e) {
   
   this.classList.toggle('fa-eye-slash');
 })
+
+// select tab
+const tab__select$ = document.querySelector.bind(document);
+const tab__select$$ = document.querySelectorAll.bind(document);
+
+const tab__selects = tab__select$$(".imformation__place-edit-select-item");
+const panel__selects = tab__select$$(".imformation__place-edit-select-panel");
+ 
+const tab__selectActive = tab__select$(".imformation__place-edit-select-item.active");
+
+tab__selects.forEach((tab__select, index__tab__select) => {
+  const panel__select = panel__selects[index__tab__select];
+
+  tab__select.onclick = function () {
+    tab__select$(".imformation__place-edit-select-item.active").classList.remove("active");
+    tab__select$(".imformation__place-edit-select-panel.active").classList.remove("active");
+
+    this.classList.add("active");
+    panel__select.classList.add("active");
+  };
+});
+
+// focus input select
+function focus__input() {
+  var input = document.getElementById('show__select');
+  var content = document.getElementById('content__select');
+  var check = document.getElementById('select__input');
+
+  input.onfocus = function() {
+    content.style.display = 'block';
+  }
+
+  document.addEventListener('click', (evt) => {
+    var bo1 = input.contains(evt.target);
+    var bo2 = content.contains(evt.target);
+
+    if(!bo1 && !bo2) {
+      content.style.display = 'none';
+    }
+  })
+}
+focus__input();
+
+function check__con(e) {
+  var id__get = document.getElementById(e.id);
+
+  if(!id__get.classList.contains('active')) {
+    reset__con();
+    id__get.classList.add('active');
+  };
+}
+
+function reset__con() {
+  var list = document.getElementsByClassName('imformation__place-edit-select-con-item');
+
+  for(var i = 0; i < list.length; i++) {
+    list[i].classList.remove('active')
+  }
+}
+
+function check__dis(e) {
+  var id__get = document.getElementById(e.id);
+
+  if(!id__get.classList.contains('active')) {
+    reset__dis();
+    id__get.classList.add('active');
+  };
+}
+
+function reset__dis() {
+  var list = document.getElementsByClassName('imformation__place-edit-select-dis-item');
+
+  for(var i = 0; i < list.length; i++) {
+    list[i].classList.remove('active')
+  }
+}
+
+function check__com(e) {
+  var id__get = document.getElementById(e.id);
+
+  if(!id__get.classList.contains('active')) {
+    reset__com();
+    id__get.classList.add('active');
+  };
+}
+
+function reset__com() {
+  var list = document.getElementsByClassName('imformation__place-edit-select-com-item');
+
+  for(var i = 0; i < list.length; i++) {
+    list[i].classList.remove('active')
+  }
+}
+
+function ac__btn(e) {
+  var get = document.getElementById(e.id);
+  var id = e.id;
+  var id__sp = id.split('__');
+  var show = document.getElementById(`${id__sp[0]}__${id__sp[1]}__show`);
+  console.log(show)
+  if(!get.classList.contains('active')) {
+    ac__reset();
+    get.classList.add('active');
+    show.classList.add('active');
+  }
+
+} 
+function ac__reset() {
+  var list = document.getElementsByClassName('imformation__place-detail-item-ac-set');
+
+  for(var i = 0; i < list.length; i++) {
+    list[i].classList.remove('active')
+  }
+
+  var list2 = document.getElementsByClassName('imformation__place-detail-item-td-btn');
+
+  for(var i = 0; i < list2.length; i++ ) {
+    list2[i].classList.remove('active');
+  }
+}
